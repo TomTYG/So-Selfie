2a68188c4511c8ec23ba2cc13b4353c0360b97fc
TomTYG <tom@tygdigital.com>
2014-02-18 10:05:13 +0100

    Initial commit

---
+++ soselfie/soselfie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  soselfie
+//
+//  Created by Tom van Kruijsbergen on 18/02/14.
+//  Copyright (c) 2014 TYG Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ soselfie/soselfieTests/soselfieTests.m
@@ -0,0 +1,34 @@
+//
+//  soselfieTests.m
+//  soselfieTests
+//
+//  Created by Tom van Kruijsbergen on 18/02/14.
+//  Copyright (c) 2014 TYG Digital. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface soselfieTests : XCTestCase
+
+@end
+
+@implementation soselfieTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
